005b89cfa63597593b48eeadf03cd3713fee1f04
G-PPeralta <prates_peralta@hotmail.com>
2023-01-18 09:42:44 -0300

    feat: install zod and create schema for post of habit

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,26 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "./lib/prisma";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/habits", async () => {
-    const habits = await prisma.habit.findMany();
+  app.post("/habits", async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
 
-    return habits;
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+        weekDays: {
+          create: weekDays.map((weekDay) => ({
+            week_day: weekDay,
+          })),
+        },
+      },
+    });
   });
 }
