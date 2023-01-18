70776427c9598590393fe3fe600bb8c0d9b8d0ee
G-PPeralta <prates_peralta@hotmail.com>
2023-01-18 15:27:15 -0300

    feat: increments get habit route to return completed habits

--- src/routes.ts
+++ src/routes.ts
@@ -36,6 +36,7 @@ export async function appRoutes(app: FastifyInstance) {
 
     const { date } = getDayParams.parse(request.query);
 
+    const parsedDate = dayjs(date).startOf("day");
     const weekDay = dayjs(date).get("day");
 
     const possibleHabits = await prisma.habit.findMany({
@@ -51,6 +52,19 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
 
-    return { possibleHabits };
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return { possibleHabits, completedHabits };
   });
 }
